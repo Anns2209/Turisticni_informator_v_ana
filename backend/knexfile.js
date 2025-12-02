b1fa345df96c677e562b143f9b455a6fdecb844f
Anns2209 <ana.gjorcheska@student.um.si>
2025-12-02 23:38:58 +0100

    Add files via upload

---
+++ backend/knexfile.js
@@ -0,0 +1,9 @@
+﻿require("dotenv").config();
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: "./migrations" },
+    seeds: { directory: "./seeds" }
+  }
+};
